6ec3e37864428d13dfa1e7de20e486e5843b714a
Michael Limberger <contact@mikelimberger.com>
2017-06-19 09:22:46 -0400

    Initial Commit

---
+++ FIX_DRUPAL_3.0_FLAW.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+
+#!/usr/bin/ruby
+
+path = nil
+
+DATA.each do |line|
+
+	line.chomp!
+	
+	path = line =~ %r{^PATH} ? line.split(':')[1] : '/var/www/drupal/8'
+	next if line =~ %r{^PATH} || line =~ /^\#/
+	
+	cmd = %Q{cd #{path} && #{line}}
+	puts %Q{EXECUTING: #{cmd}}
+	system cmd
+	# if (line =~ /^drush\ up/)
+	#	puts %Q{Update Complete. Press Enter To Return...}
+	#	STDIN.gets
+	# end
+end
+
+__END__
+PATH:/var/www/drupal/8
+drush cr
+drush updb -y
+drush entup -y
+drush up -y && drush cr
+drush pmu ds -y
+drush pmu layout_plugin -y
+drush cr
+drush dl ds-8.x-3.1 -y
+drush en ds -y 
+drush dl radix_layouts-8.x-4.0 -y
+drush en radix_layouts -y
+drush updb -y
+drush entup -y
+drush cr
